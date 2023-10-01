425e40748f11f4f83eda71a9c79436da33942350
luizamfsantos <luiza.marques_@hotmail.com>
2023-10-01 14:07:34 -0300

    feature: tests for limpeza_dados.py

--- tests/limpeza_tests.py
+++ tests/limpeza_tests.py
@@ -18,22 +18,23 @@ from src.preprocessamento_de_dados.limpeza_dados import *
 def check_report(df):
     # check if df["report"] == "METAF"
     if "METAF" not in df["report"].values:
-        ValueError("METAF not found in df['report']")
+        raise ValueError("METAF not found in df['report']")
 
 if __name__ == "__main__":
-    data_check_report = {
-        'hora': [1654041600000, 1654045200000],
-        'report': ['METAR SBGL 010000Z  25002KT 3000     BR OVC033   23/21 Q1016=', 'METAR SBGL 010100Z 02001KT 2000     BR OVC011   22/21 Q1017='],
-        'station': ['SBGL', 'SBGL'],
-        'dt_origin': ['010000Z', '010100Z'],
-        'wind': ['25002KT', '02001KT'],
-        'visibility': [3000, 2000],
-        'weather': ['BR', 'BR'],
-        'clouds': ['OVC033', 'OVC011'],
-        'temperature': ['23/21', '22/21'],
-        'dew_point': ['21', '21'],
-        'altimeter (hPA)': ['Q1016=', 'Q1017='],
-        'aero': ['NaN', 'NaN']
-    }
-    df_check_report = pd.DataFrame(data_check_report)
+    # data_check_report = {
+    #     'hora': [1654041600000, 1654045200000],
+    #     'report': ['METAR', 'METAR'],
+    #     'station': ['SBGL', 'SBGL'],
+    #     'dt_origin': ['010000Z', '010100Z'],
+    #     'wind': ['25002KT', '02001KT'],
+    #     'visibility': [3000, 2000],
+    #     'weather': ['BR', 'BR'],
+    #     'clouds': ['OVC033', 'OVC011'],
+    #     'temperature': ['23/21', '22/21'],
+    #     'dew_point': ['21', '21'],
+    #     'altimeter (hPA)': ['Q1016=', 'Q1017='],
+    #     'aero': ['SBGL', 'SBSG']}
+    # df_check_report = pd.DataFrame(data_check_report)
+    # check_report(df_check_report)
+
 
